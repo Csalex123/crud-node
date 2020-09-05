4e0f4787ebada7560b9ad362d716cde63dbb3c3f
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-09-05 11:23:15 -0300

    Get: todos os usuários

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const mysql = require('mysql');
+const bodyparser = require('body-parser');
+
+const app = express();
+
+app.use(bodyparser.json());
+
 
 const mysqlConnection = mysql.createConnection({
     host: 'localhost',
@@ -12,6 +18,20 @@ mysqlConnection.connect((err) => {
     if (!err) {
         console.log('DB connection succeded');
     }else {
-        console.log(`DB connection falied \n Error: ${JSON.stringify(err)}`)
+        console.log(`DB connection falied \n Error: ${JSON.stringify(err, undefined, 2)}`)
     }
+});
+
+
+app.listen(3000);
+
+// Get All users
+app.get('/users', (req, res) => {
+    mysqlConnection.query('SELECT * FROM user', (err, rows, fields) => {
+        if(!err){
+            res.send(rows);
+        }else{
+            console.log(err)
+        }
+    });
 });
